29c93971c4fb907bdf1ef050dcde3aebbaf6362e
Maria Shirokova <m.shirokova@space307.com>
2022-11-15 15:32:12 +0300

    initialized project

---
+++ spb_subway/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spb_subway
+//
+//  Created by m.shirokova on 15.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
